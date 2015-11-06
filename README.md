29a58a12e61f8db0f65bbf467dedd9156438311e
Stefan Morgenthaler <stefan+dev@morgenthaler.at>
2015-11-06 22:58:42 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# bashpool #
+
+Bashpool, a collection of scripts "grown" over the past few years and are worth (IMHO) to share with you folks.
+
+## Scripts Overview ##
+
+| name         | sourceable | executable |          description         |
+|:-------------|:----------:|:----------:|------------------------------|
+| | | | |
+
+## License ##
+
+If not otherwise mentioned, GPLv3 applies.
---
+++ tests/runTests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+: ${BASEDIR:=$(cd $(dirname $0)/../ && pwd)}
+
+test -d ${BASEDIR/tmp} \
+  && rm -rf ${BASEDIR}/tmp
+
+mkdir -p ${BASEDIR}/tmp/shunit2 \
+  && curl -Ls "https://shunit2.googlecode.com/files/shunit2-2.1.6.tgz" | tar zx -C tmp/shunit2 --strip-components=1
+
+suite () {
+  for test_file in $(\ls tests/*.test); do
+    source ${test_file}
+    test_methods="$(_shunit_extractTestFunctions ${test_file})"
+    for test_method in ${test_methods}; do
+      suite_addTest ${test_method}
+    done
+  done
+}
+
+source tmp/shunit2/src/shunit2
